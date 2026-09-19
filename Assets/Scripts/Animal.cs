5b00c7c9556e2c081d51fd801f96a76370c4e859
pykmso81 <pykmso81@gmail.com>
2026-09-19 18:08:56 +0300

    inheretance works

---
+++ Assets/Scripts/Animal.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+    public void Eat()
+    {
+        Debug.Log("Animal is eating");
+    }
+}
---
+++ Assets/Scripts/Cat.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Cat : Animal
+{
+    void Start()
+    {
+        Eat();
+    }
+}
